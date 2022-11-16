0b4b7612f5fa8b05ae85414e20525b10865ac218
lunalo57 <lunalo.dev@gmail.com>
2022-11-16 23:02:09 +0100

    function email checker done

--- script.js
+++ script.js
@@ -2,11 +2,35 @@ const inputs = document.querySelectorAll(
   'input[type="text"] , input[type="password"]'
 );
 
+let pseudo , email , password , confirmPass;
 
+const errorDisplay = (tag , message , valid) => {
+    const container = document.querySelector("." + tag + "-container");
+    const span = document.querySelector("." + tag + "-container > span");
 
+    if(!valid){
+        container.classList.add("error");
+        span.textContent = message;
+    }else{
+        container.classList.remove("error");
+        span.textContent = message;
+    }
+}
 
+const pseudoChecker = (value) => {
+    if(value.length > 0 && (value.length < 3 || value.length > 20)){
+        errorDisplay("pseudo" , "le pseudo doit faire entre 3 et 20 caractères");
+        pseudo = null;
+    }else if(!value.match(/^[a-zA-Z0-9_.-]*$/)){
+        errorDisplay("pseudo" , "le pseudo ne doit pas contenir de caractères spéciaux");
+        pseudo = null;
+    }else{
+        errorDisplay("pseudo" , "" , true);
+        pseudo = value;
+    }
+}
 
-//y Cette focntion est trop grosse et répétitive, on va donc créer une
+//y Cette fonction est trop grosse et répétitive, on va donc créer une
 //y une fonction beaucoup plus simple comprenant 3 paramètres
 //v const pseudoChecker = (value) => {
 //v   const pseudoContainer = document.querySelector(".pseudo-container");
@@ -25,7 +49,13 @@ const inputs = document.querySelectorAll(
 //v };
 
 const emailChecker = (value) => {
-  console.log(value);
+    if(!value.match(/^[\w-_]+@[\w-]+\.[a-z]{2,4}$/i)){
+        errorDisplay("email" , "le mail n'est pas valide"),
+        email = null;
+    }else{
+        errorDisplay("email" , "" , true);
+        email = value;
+    }
 };
 
 const passwordChecker = (value) => {
